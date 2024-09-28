d7a26f00db1ee0761a3789c21c44bfbc81c3a898
Damian <dolotd98@gmail.com>
2024-09-28 17:53:05 +0200

    change mistakes in file names

---
+++ src/main/java/com/hire_me/chat/Repository/AppUserRepo.java
@@ -0,0 +1,7 @@
+package com.hire_me.chat.Repository;
+
+import com.hire_me.chat.Entity.AppUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppUserRepo extends CrudRepository<Integer, AppUser> {
+}
--- src/main/java/com/hire_me/chat/Repository/UserRepo.java
+++
@@ -1,6 +0,0 @@
-//package com.hire_me.chat.Repository;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface UserRepo implements CrudRepository<Integer,User> {
-//}
