b6a83e4a22f4dd675572f04154df37b2afaf5ea3
Damian <dolotd98@gmail.com>
2024-09-28 20:10:50 +0200

    controller debug

--- src/main/java/com/hire_me/chat/Controllers/AppUserController.java
+++ src/main/java/com/hire_me/chat/Controllers/AppUserController.java
@@ -1,9 +1,46 @@
 package com.hire_me.chat.Controllers;
 
+import com.hire_me.chat.Entity.AppUser;
+import com.hire_me.chat.Repository.AppUserRepo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AppUserController {
+
+    private AppUserRepo appUserRepo;
+
+    public AppUserController(AppUserRepo appUserRepo) {
+        this.appUserRepo = appUserRepo;
+    }
+
+    @GetMapping("/user/{name}")
+    public ResponseEntity<AppUser> test(@PathVariable String name){
+        AppUser user = new AppUser(1,name);
+        return ResponseEntity.ok(appUserRepo.save(user));
+    }
+
+    @GetMapping("/findall")
+    public List<AppUser> test2(){
+
+        return appUserRepo.findAll();
+    }
+
+    @GetMapping("/cos")
+    public String test3(){
+
+        return "Damian";
+    }
+
+
+
+
+
 
 
 
--- src/main/java/com/hire_me/chat/Entity/AppUser.java
+++ src/main/java/com/hire_me/chat/Entity/AppUser.java
@@ -15,6 +15,8 @@ public class AppUser {
     @Id
     Integer Id;
 
+    String name;
+
 
 
 
