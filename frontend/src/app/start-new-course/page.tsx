3e694b6ba6628c0c6f93b7b66b9ea5de68a62e63
Michal Lata <michallata.dev@gmail.com>
2024-09-28 17:28:45 +0200

    feat: Add boilerplate chat page

--- frontend/src/app/start-new-course/page.tsx
+++ frontend/src/app/start-new-course/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { useChat } from "ai/react";
+
 export default function StartNewCoursePage() {
-  return <div>Start new course – CHAT</div>;
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  return (
+    <section className="flex h-dvh flex-col items-center justify-center">
+      <ul>
+        {messages.map((message) => (
+          <li key={message.id}>
+            {message.role}: {message.content}
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input value={input} type="text" onChange={handleInputChange} />
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  );
 }
